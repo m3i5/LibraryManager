8637e3c3383a237cc8665b462b502622e5c158f4
m3i5 <jakub.h.zawadzki@gmail.com>
2025-07-01 16:03:59 +0100

    add report and sql creation script

---
+++ init_db.sql
@@ -0,0 +1,30 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Resources] (
+    [Id] int NOT NULL IDENTITY,
+    [Title] nvarchar(max) NOT NULL,
+    [Author] nvarchar(max) NOT NULL,
+    [PublicationYear] int NOT NULL,
+    [Genre] nvarchar(max) NOT NULL,
+    [ResourceType] nvarchar(max) NOT NULL,
+    [IsAvailable] bit NOT NULL,
+    [BorrowedBy] nvarchar(max) NULL,
+    [DueDate] datetime2 NULL,
+    CONSTRAINT [PK_Resources] PRIMARY KEY ([Id])
+);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250629220306_InitialCreate', N'9.0.6');
+
+COMMIT;
+GO
+
